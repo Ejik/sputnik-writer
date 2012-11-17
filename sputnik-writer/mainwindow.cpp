39c110936a52574c9343b9d8dcf3eb3dc1abf48b
Sergii <sergii.kiselev@gmail.com>
2012-11-17 15:14:29 +0400

    Запоминаются все свойства

--- sputnik-writer/mainwindow.cpp
+++ sputnik-writer/mainwindow.cpp
@@ -35,8 +35,13 @@ void MainWindow::read_settings()
     QCoreApplication::setApplicationName("sputnik-writer");
 
     QSettings settings;
+
     ui->lineEdit->setText(settings.value("Раздел").toString());
     ui->lineEdit_2->setText(settings.value("Рубрика").toString());
+    ui->lineEdit_3->setText(settings.value("Ключевое").toString());
+    ui->textEdit->setText(settings.value("Текст").toString());
+    ui->lineEdit_4->setText(settings.value("Телефон").toString());
+    ui->textEdit_2->setText(settings.value("ФИО").toString());
 }
 
 void MainWindow::write_settings()
@@ -44,4 +49,8 @@ void MainWindow::write_settings()
     QSettings settings;
     settings.setValue("Раздел", ui->lineEdit->text());
     settings.setValue("Рубрика", ui->lineEdit_2->text());
+    settings.setValue("Ключевое", ui->lineEdit_3->text());
+    settings.setValue("Текст", ui->textEdit->toPlainText());
+    settings.setValue("Телефон", ui->lineEdit_4->text());
+    settings.setValue("ФИО", ui->textEdit_2->toPlainText());
 }
